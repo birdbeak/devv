84e998527c1785f7376b6be9673b6956445b241f
birdbeak <dlcksdn1148@naver.com>
2016-10-12 23:01:16 +0900

    lol

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 3000;
-var server = app.listen(port);
+var server = app.listen(process.env.PORT || 8080);
 
 app.use(express.static('public'));
 
